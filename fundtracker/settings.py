29f20ebbaf7757db03576a7f845f81ece4f6ee05
Ali Kemal Yavas <115250112+Kemalyavas@users.noreply.github.com>
2025-03-19 00:26:50 +0300

    make settings.py more secure

--- fundtracker/settings.py
+++ fundtracker/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,11 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mb-yz1_&at0$puuk+v4ffo4z#9(gy_ido%h=v%@*31duy4tqf-'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-mb-yz1_&at0$puuk+v4ffo4z#9(gy_ido%h=v%@*31duy4tqf-')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 ALLOWED_HOSTS = []
 
 
